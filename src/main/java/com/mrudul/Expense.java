d0693cd282f0694a78009b2174a24d13222930e2
Mrudul <mrudulnagrale01@gmail.com>
2025-09-24 01:53:53 +0530

    feat: add Expense model.

---
+++ src/main/java/com/mrudul/Expense.java
@@ -0,0 +1,62 @@
+package com.mrudul;
+
+import java.util.Date;
+
+public class Expense {
+    private int id;
+    private String title;
+    private double amount;
+    private String category;
+    private  Date localDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate(Date localDate) {
+        this.localDate = localDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", amount=" + amount +
+                ", category='" + category + '\'' +
+                ", localDate=" + localDate +
+                '}';
+    }
+}
--- src/main/java/com/mrudul/Main.java
+++ src/main/java/com/mrudul/Main.java
@@ -1,17 +1,38 @@
 package com.mrudul;
 
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Expense expense1 = new Expense();
+        Expense expense2 = new Expense();
+        Expense expense3 = new Expense();
+
+        expense1.setId(1);
+        expense1.setAmount(100);
+        expense1.setCategory("Food");
+        expense1.setTitle("Kartoffel");
+        expense1.setLocalDate(new Date());
+
+
+        expense2.setId(2);
+        expense2.setAmount(200);
+        expense2.setCategory("Drink");
+        expense2.setTitle("Apfelsaft");
+        expense2.setLocalDate(new Date());
+
+
+        expense3.setId(3);
+        expense3.setAmount(300);
+        expense3.setCategory("Sweet");
+        expense3.setTitle("Kuchen");
+        expense3.setLocalDate(new Date());
+
+        System.out.println(expense1);
+        System.out.println(expense2);
+        System.out.println(expense3);
     }
 }
