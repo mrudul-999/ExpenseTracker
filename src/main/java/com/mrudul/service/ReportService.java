bcfb1e5e9dc63ab5befe235cc4122d2af0b2aaff
Mrudul <mrudulnagrale01@gmail.com>
2025-09-24 22:23:10 +0530

    feat: add ReportService with expense analytics.

---
+++ src/main/java/com/mrudul/service/ReportService.java
@@ -0,0 +1,53 @@
+package com.mrudul.service;
+
+import com.mrudul.dao.ExpenseDAO;
+import com.mrudul.util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReportService {
+
+    double getTotalExpenses() {
+        ExpenseDAO expenseDAO = new ExpenseDAO();
+        String sql_getTotalExpense = "Select SUM(amount) as total from expense.expenses";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql_getTotalExpense);
+            ResultSet rs = preparedStatement.executeQuery();
+            double amount = 0;
+            if (rs.next())
+            {
+                return rs.getDouble("amount");
+            }
+            return amount;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    double getExpensesByCategory(String category)
+    {
+        String sql_getExpenseByCategory = "Select SUM(amount) from expense.expenses where category = ?";
+        try {
+            Connection connection = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql_getExpenseByCategory);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next())
+            {
+                return rs.getDouble("amount");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return 0;
+    }
+
+    double getMonthlyReport(int month, int year)
+    {
+        return 0;
+    }
+}
